0940b61d0f3b843cf6a0912313d6d4f11f0eb1e2
philia <imphilas@gmail.com>
2021-01-26 23:51:45 +0800

    .

--- ios/Classes/SwiftCapoTokenCorePlugin.swift
+++ ios/Classes/SwiftCapoTokenCorePlugin.swift
@@ -35,151 +35,218 @@ public class SwiftCapoTokenCorePlugin: NSObject, FlutterPlugin {
         }
     }
 
-    private func onRandomMnemonic(_ call: FlutterMethodCall, result: (Any?) -> Void) {
+    private func onRandomMnemonic(_ call: FlutterMethodCall, result:  (Any?) -> Void) {
         let mnemonic = MnemonicUtil.generateMnemonic(words: Words.twelve)
         result(mnemonic)
     }
 
-    private func onImportMnemonic(_ call: FlutterMethodCall, result: (Any?) -> Void) {
+    private func onImportMnemonic(_ call: FlutterMethodCall, result: @escaping (Any?) -> Void) {
         guard let arguments = isArgumentIllegal(call, result: result) else {
             return
         }
-        do {
-            let mnemonic = arguments["mnemonic"] as! String
-            let password = arguments["password"] as! String
-            let walletMeta = WalletMeta(chain: ChainType.eth, from: WalletFrom.mnemonic)
-            let identity = try Identity.recoverIdentity(metadata: walletMeta, mnemonic: mnemonic, password: password)
+        DispatchQueue.global().async {
+            do {
+                let mnemonic = arguments["mnemonic"] as! String
+                let password = arguments["password"] as! String
+                let walletMeta = WalletMeta(chain: ChainType.eth, from: WalletFrom.mnemonic)
+                let identity = try Identity.recoverIdentity(metadata: walletMeta, mnemonic: mnemonic, password: password)
 
-            let importResult = identity.ethereumWallet.keystore.dump()
-            result(importResult)
-        } catch {
-            result(FlutterError(code: ErrorCode.error.rawValue, message: "\(error)", details: nil))
+                let importResult = identity.ethereumWallet.keystore.dump()
+                DispatchQueue.main.async {
+                    result(importResult)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    result(FlutterError(code: ErrorCode.error.rawValue, message: "\(error)", details: nil))
+                }
+               
+            }
         }
+       
     }
 
-    private func onExportMnemonic(_ call: FlutterMethodCall, result: (Any?) -> Void) {
+    private func onExportMnemonic(_ call: FlutterMethodCall, result:@escaping (Any?) -> Void) {
         guard let arguments = isArgumentIllegal(call, result: result) else {
             return
         }
-        do {
-            let keystore = arguments["keystore"] as! String
-            let password = arguments["password"] as! String
+        DispatchQueue.global().async {[weak self] in
+            guard let self = self else{return}
+            do {
+                let keystore = arguments["keystore"] as! String
+                let password = arguments["password"] as! String
 
-            let jsonData: Data? = keystore.data(using: .utf8)
-            let json = try JSONSerialization.jsonObject(with: jsonData!) as! JSONObject
-            guard let _ = json["encMnemonic"] else {
-                result(FlutterError(code: ErrorCode.illegalOperation.rawValue, message: "The keystore does not have mnemonic.", details: nil))
-                return
+                let jsonData: Data? = keystore.data(using: .utf8)
+                let json = try JSONSerialization.jsonObject(with: jsonData!) as! JSONObject
+                guard let _ = json["encMnemonic"] else {
+                    DispatchQueue.main.async {
+                        result(FlutterError(code: ErrorCode.illegalOperation.rawValue, message: "The keystore does not have mnemonic.", details: nil))
+                    }
+                   
+                    return
+                }
+
+                let mapResult = try self.mapKeystoreString2Object(json: json)
+                if let identityKeystore = mapResult.0 {
+                    let mnemonic: String = try identityKeystore.mnemonic(from: password)
+                    result(mnemonic)
+                }
+
+                if let wallet = mapResult.1 {
+                    let mnemonic: String = try wallet.exportMnemonic(password: password)
+                    DispatchQueue.main.async {
+                        result(mnemonic)
+                    }
+                   
+                }
+
+            } catch {
+                DispatchQueue.main.async {
+                    result(FlutterError(code: ErrorCode.exportError.rawValue, message: "\(error.localizedDescription)", details: nil))
+                }
+               
             }
-
-            let mapResult = try mapKeystoreString2Object(json: json)
-            if let identityKeystore = mapResult.0 {
-                let mnemonic: String = try identityKeystore.mnemonic(from: password)
-                result(mnemonic)
-            }
-
-            if let wallet = mapResult.1 {
-                let mnemonic: String = try wallet.exportMnemonic(password: password)
-                result(mnemonic)
-            }
-
-        } catch {
-            result(FlutterError(code: ErrorCode.exportError.rawValue, message: "\(error.localizedDescription)", details: nil))
         }
+       
     }
 
-    private func onImportKeystore(_ call: FlutterMethodCall, result: (Any?) -> Void) {
+    private func onImportKeystore(_ call: FlutterMethodCall, result: @escaping (Any?) -> Void) {
         guard let arguments = isArgumentIllegal(call, result: result) else {
             return
         }
-        do {
-            let keystore = arguments["keystore"] as! String
-            let password = arguments["password"] as! String
-            let mapResult = try mapKeystoreString2Object(json: nil, keystoreString: keystore)
+        DispatchQueue.global().async {[weak self] in
+            guard let self = self else {return}
+            do {
+                let keystore = arguments["keystore"] as! String
+                let password = arguments["password"] as! String
+                let mapResult = try self.mapKeystoreString2Object(json: nil, keystoreString: keystore)
 
-            if let wallet = mapResult.1 {
-                let privateKey: String = try wallet.privateKey(password: password, isHDWalletExportWif: false)
+                if let wallet = mapResult.1 {
+                    let privateKey: String = try wallet.privateKey(password: password, isHDWalletExportWif: false)
 
-                print("privateKey: \(privateKey)")
-                let walletMeta = WalletMeta(chain: ChainType.eth, from: WalletFrom.keystore)
+                    let walletMeta = WalletMeta(chain: ChainType.eth, from: WalletFrom.keystore)
+                    let wallet = try BasicWallet.importFromPrivateKey(privateKey, encryptedBy: password, metadata: walletMeta)
+
+                    let keystore: String = wallet.keystore.dump()
+                    DispatchQueue.main.async {
+                        result(keystore)
+                    }
+                   
+                }
+
+                if let _ = mapResult.0 {
+                    DispatchQueue.main.async {
+                        result(FlutterError(code: ErrorCode.illegalOperation.rawValue, message: "Keystore type not match..", details: nil))
+                    }
+                   
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    result(FlutterError(code: ErrorCode.error.rawValue, message: "\(error)", details: nil))
+                }
+               
+            }
+        }
+       
+    }
+
+    private func onImportPrivateKey(_ call: FlutterMethodCall, result: @escaping (Any?) -> Void) {
+        guard let arguments = isArgumentIllegal(call, result: result) else {
+            return
+        }
+        DispatchQueue.global().async {
+            do {
+                let privateKey = arguments["privateKey"] as! String
+                let password = arguments["password"] as! String
+
+                let walletMeta = WalletMeta(chain: ChainType.eth, from: WalletFrom.privateKey)
                 let wallet = try BasicWallet.importFromPrivateKey(privateKey, encryptedBy: password, metadata: walletMeta)
 
                 let keystore: String = wallet.keystore.dump()
-                result(keystore)
-            }
+                DispatchQueue.main.async {
+                    result(keystore)
 
-            if let _ = mapResult.0 {
-                result(FlutterError(code: ErrorCode.illegalOperation.rawValue, message: "Keystore type not match..", details: nil))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    result(FlutterError(code: ErrorCode.error.rawValue, message: "\(error)", details: nil))
+                }
+               
             }
-        } catch {
-            result(FlutterError(code: ErrorCode.error.rawValue, message: "\(error)", details: nil))
         }
+       
     }
 
-    private func onImportPrivateKey(_ call: FlutterMethodCall, result: (Any?) -> Void) {
+    private func onExportPrivateKey(_ call: FlutterMethodCall, result: @escaping (Any?) -> Void) {
         guard let arguments = isArgumentIllegal(call, result: result) else {
             return
         }
-        do {
-            let privateKey = arguments["privateKey"] as! String
-            let password = arguments["password"] as! String
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            do {
+                let keystore = arguments["keystore"] as! String
+                let password = arguments["password"] as! String
+                let mapResult = try self.mapKeystoreString2Object(json: nil, keystoreString: keystore, isExportPrivate: true)
 
-            let walletMeta = WalletMeta(chain: ChainType.eth, from: WalletFrom.privateKey)
-            let wallet = try BasicWallet.importFromPrivateKey(privateKey, encryptedBy: password, metadata: walletMeta)
+                if let wallet = mapResult.1 {
+                    let privateKey: String = try wallet.privateKey(password: password, isHDWalletExportWif: true)
+                    DispatchQueue.main.async {
+                        result(privateKey)
+                    }
+                }
 
-            let keystore: String = wallet.keystore.dump()
-            result(keystore)
-        } catch {
-            result(FlutterError(code: ErrorCode.error.rawValue, message: "\(error)", details: nil))
+                if let _ = mapResult.0 {
+                    DispatchQueue.main.async {
+                        result(FlutterError(code: ErrorCode.illegalOperation.rawValue, message: "Identity doesn't support export private key.", details: nil))
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    result(FlutterError(code: ErrorCode.exportError.rawValue, message: "\(error)", details: nil))
+                }
+            }
         }
     }
 
-    private func onExportPrivateKey(_ call: FlutterMethodCall, result: (Any?) -> Void) {
+    private func onVerityPassword(_ call: FlutterMethodCall, result:@escaping (Any?) -> Void) {
         guard let arguments = isArgumentIllegal(call, result: result) else {
             return
         }
-        do {
-            let keystore = arguments["keystore"] as! String
-            let password = arguments["password"] as! String
-            let mapResult = try mapKeystoreString2Object(json: nil, keystoreString: keystore,isExportPrivateKey: true)
+        DispatchQueue.global().async {[weak self] in
+            guard let self = self else{return}
+            do {
+                let keystore = arguments["keystore"] as! String
+                let password = arguments["password"] as! String
 
-            if let wallet = mapResult.1 {
-                let privateKey: String = try wallet.privateKey(password: password, isHDWalletExportWif: true)
-                result(privateKey)
-            }
+                let mapResult = try self.mapKeystoreString2Object(json: nil, keystoreString: keystore)
+                if let identityKeystore = mapResult.0 {
+                    let verify: Bool = identityKeystore.verify(password: password)
+                    DispatchQueue.main.async {
+                        result("\(verify)")
+                    }
+                   
+                    return
+                }
 
-            if let _ = mapResult.0 {
-                result(FlutterError(code: ErrorCode.illegalOperation.rawValue, message: "Identity doesn't support export private key.", details: nil))
+                if let wallet = mapResult.1 {
+                    let verify: Bool = wallet.verifyPassword(password)
+                    DispatchQueue.main.async {
+                        result("\(verify)")
+                    }
+                   
+                    return
+                }
+                DispatchQueue.main.async {
+                    result(FlutterError(code: ErrorCode.keystoreError.rawValue, message: "Keystore type not match.", details: nil))
+                }
+               
+            } catch {
+                DispatchQueue.main.async {
+                    result(FlutterError(code: ErrorCode.keystoreError.rawValue, message: "\(error)", details: nil))
+                }
+               
             }
-        } catch {
-            result(FlutterError(code: ErrorCode.exportError.rawValue, message: "\(error)", details: nil))
         }
-    }
-
-    private func onVerityPassword(_ call: FlutterMethodCall, result: (Any?) -> Void) {
-        guard let arguments = isArgumentIllegal(call, result: result) else {
-            return
-        }
-        do {
-            let keystore = arguments["keystore"] as! String
-            let password = arguments["password"] as! String
-
-            let mapResult = try mapKeystoreString2Object(json: nil, keystoreString: keystore)
-            if let identityKeystore = mapResult.0 {
-                let verify: Bool = identityKeystore.verify(password: password)
-                result("\(verify)")
-                return
-            }
-
-            if let wallet = mapResult.1 {
-                let verify: Bool = wallet.verifyPassword(password)
-                result("\(verify)")
-                return
-            }
-            result(FlutterError(code: ErrorCode.keystoreError.rawValue, message: "Keystore type not match.", details: nil))
-        } catch {
-            result(FlutterError(code: ErrorCode.keystoreError.rawValue, message: "\(error)", details: nil))
-        }
+       
     }
 
     // only accept map param
@@ -195,27 +262,27 @@ public class SwiftCapoTokenCorePlugin: NSObject, FlutterPlugin {
         return arguments
     }
 
-    func mapKeystoreString2Object(json: JSONObject?, keystoreString: String? = nil,isExportPrivateKey:Bool = false) throws -> (IdentityKeystore?, BasicWallet?) {
+    func mapKeystoreString2Object(json: JSONObject?, keystoreString: String? = nil, isExportPrivate: Bool = false) throws -> (IdentityKeystore?, BasicWallet?) {
         var theJson: JSONObject
         if let keystoreStr = keystoreString {
             let jsonData: Data? = keystoreStr.data(using: .utf8)
             let json = try JSONSerialization.jsonObject(with: jsonData!) as! JSONObject
             theJson = json
         } else {
-            
             theJson = json!
         }
         if let _ = theJson["identifier"] {
             let identityKeystore = try IdentityKeystore(json: theJson)
             return (identityKeystore, nil)
         } else {
-            if((theJson[WalletMeta.key] as? JSONObject) == nil){
+            if (theJson[WalletMeta.key] as? JSONObject) == nil {
                 let mata = WalletMeta(chain: ChainType.eth, from: WalletFrom.keystore, network: Network.mainnet, segwit: SegWit.none)
                 theJson[WalletMeta.key] = mata.toJSON()
             }
-            
-            let wallet = try BasicWallet(json: theJson,isExportPrivateKey: isExportPrivateKey)
+
+            let wallet = try BasicWallet(json: theJson)
             return (nil, wallet)
         }
     }
 }
+
